cbc1db3c0ba861e975b96808af30806814b59663
Andres Patarroyo <A@gmail.com>
2024-05-02 10:35:16 -0500

    Adelanto taller

--- src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/Circulo.java
@@ -1,5 +1,38 @@
 package com.mycompany.figurasgeometricaspoo;
 
-public class Circulo {
+public class Circulo extends FiguraGeometrica {
     
+    private double radio;
+    
+    /**
+     * Constructor de la clase circulo.
+     *
+     * @param nombre
+     * @param color
+     * @param radio
+     *
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    
+    public Circulo(String nombre, String color, double radio) {
+        super(nombre, color);
+        this.radio = radio;
+    }
+
+    /**
+     * Método para imprimir en consola los datos del circulo.
+     * Redefine el método mostrarInfo de la clase padre FiguraGeometrica.
+     * 
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+
+    public double obtenerArea() {
+        return super.obtenerArea();
+
+    }
+    public double obtenerPerimetro() {
+        return super.obtenerPerimetro();
+    }
 }
+    
+
--- src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
+++ src/main/java/com/mycompany/figurasgeometricaspoo/FiguraGeometrica.java
@@ -1,5 +1,44 @@
 package com.mycompany.figurasgeometricaspoo;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * Clase para una prenda de vestir.
+ */
 public class FiguraGeometrica {
+    private String nombre;
+    private String color;
+
+    /**
+     * Constructor de la clase.
+     * 
+     * @param nombre nombre de la figura
+     * @param color color de la figura
+     *
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    public FiguraGeometrica (String nombre,String color){
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    /**
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    public double obtenerArea() {
+        
+
+        return precioFormateado;
+    }
+
+    /**
+     * 
+     * Complejidad temporal: O(1) Tiempo constante.
+     */
+    public double obtenerPerimetro() {
+ ;
+    }
+}   
     
-}
+
